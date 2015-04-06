f85a35058b712396386494662fbab8ae30faadde
Ben Sower <sauer.benjamin@gmail.com>
2015-04-07 01:48:27 +0200

    installation added

--- sublime-profile.py
+++ sublime-profile.py
@@ -1,6 +1,6 @@
 import sublime
 import sublime_plugin
-import os
+import os, shutil
 import http.client
 import json
 
@@ -29,7 +29,7 @@ Save it or use it with the 'Import' function!""" % response_parsed['_id'])
         headers = {'Content-Type': 'application/json'}
         connection = http.client.HTTPSConnection(self.server_url)
         json_string = json.dumps(json_file)
-        # print(json_string)
+        #print(json_string)
 
         connection.request('POST', self.api_path, json_string, headers)
         response = connection.getresponse()
@@ -44,8 +44,23 @@ class import_profileCommand(sublime_plugin.TextCommand):
         self.view.window().show_input_panel('Please enter the profile id', '', self.importProfile, None, None)
 
     def importProfile(self, id):
+        user_folder_path = os.path.join(sublime.packages_path(), "User")
+        installed_packages_path = os.path.join(
+            user_folder_path, "Package Control.sublime-settings")
+        
+        #create a backup of the settings file
+        shutil.copy2(installed_packages_path, installed_packages_path + ".backup")
+
+        # import saved profile
         connection = http.client.HTTPSConnection(self.server_url)
         connection.request('GET', self.api_path + '/' + id)
-        response = connection.getresponse()
-        print (json.loads(response.read().decode())['installed_packages'])
+        imported_settings = connection.getresponse().read().decode()
+        imported_settings = json.loads(imported_settings)
+        #print (imported_settings)
 
+        #overwrite old settings file
+        with open(installed_packages_path, 'w') as outfile:
+            json.dump(imported_settings, outfile, indent = 4)
+
+
+# 552316a8e459a40300f6a4b7
